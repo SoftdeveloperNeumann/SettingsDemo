f7018b20c93d30e3a6880e312d2728ad942c25ac
Frank <f.neumann.berlin@googlemail.com>
2023-01-26 12:57:53 +0100

    Settings bis Widget

--- app/src/main/java/com/example/settingsdemo/settings/DialogFragment.kt
+++ app/src/main/java/com/example/settingsdemo/settings/DialogFragment.kt
@@ -2,9 +2,10 @@ package com.example.settingsdemo.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.example.settingsdemo.R
 
 class DialogFragment: PreferenceFragmentCompat(){
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-
+        setPreferencesFromResource(R.xml.dialog_preferences,rootKey)
     }
 }
--- app/src/main/java/com/example/settingsdemo/settings/WidgetFragment.kt
+++ app/src/main/java/com/example/settingsdemo/settings/WidgetFragment.kt
@@ -2,9 +2,10 @@ package com.example.settingsdemo.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.example.settingsdemo.R
 
 class WidgetFragment:PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-
+        setPreferencesFromResource(R.xml.widget_preferences,rootKey)
     }
 }
