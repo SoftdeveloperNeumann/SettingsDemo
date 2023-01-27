36260e4aef6293ebe10469601f4058e08960b9ad
Frank <f.neumann.berlin@googlemail.com>
2023-01-27 09:10:21 +0100

    Settings Fertig

--- app/src/main/java/com/example/settingsdemo/MainActivity.kt
+++ app/src/main/java/com/example/settingsdemo/MainActivity.kt
@@ -3,19 +3,43 @@ package com.example.settingsdemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.preference.PreferenceManager
 import com.example.settingsdemo.databinding.ActivityMainBinding
 import com.example.settingsdemo.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private  val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val prefs = PreferenceManager.getDefaultSharedPreferences(application)
+
+        val values = prefs.all
+
+        for((key,value) in values){
+            Log.d(TAG, "onCreate: $key -> $value")
+        }
+
+        binding.tvOutput.text = prefs.getString("user_name","defaultwert")
+
+        binding.btnSetName.setOnClickListener {
+            prefs.edit().putString("user_name", binding.etName.text.toString())
+            .apply()
+            binding.tvOutput.text = prefs.getString("user_name","Default")
+        }
+
+        binding.btnDelete.setOnClickListener {
+            prefs.edit().remove("user_name").apply()
+            binding.tvOutput.text = prefs.getString("user_name","Default")
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/settingsdemo/settings/AdvancedFragment.kt
+++ app/src/main/java/com/example/settingsdemo/settings/AdvancedFragment.kt
@@ -2,9 +2,11 @@ package com.example.settingsdemo.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.example.settingsdemo.R
+
 
 class AdvancedFragment:PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-
+        setPreferencesFromResource(R.xml.advanced_preferences,rootKey)
     }
 }
