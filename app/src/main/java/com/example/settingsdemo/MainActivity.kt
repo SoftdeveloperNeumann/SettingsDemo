f3b525f0d6d45a462b771b0a1566bc5ec3c6b6fc
Frank <f.neumann.berlin@googlemail.com>
2023-02-08 08:39:57 +0100

    Settings Fertig

--- app/src/main/java/com/example/settingsdemo/MainActivity.kt
+++ app/src/main/java/com/example/settingsdemo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.settingsdemo
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,44 +15,54 @@ import com.example.settingsdemo.settings.SettingsActivity
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    private  val TAG = "MainActivity"
-
+    private val TAG = "MainActivity"
+    lateinit var prefs: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val prefs = PreferenceManager.getDefaultSharedPreferences(application)
-
+        prefs = PreferenceManager.getDefaultSharedPreferences(application)
+        val setColor = prefs.getBoolean("check_box_preference_1",false)
         val values = prefs.all
 
-        for((key,value) in values){
+        for ((key, value) in values) {
             Log.d(TAG, "onCreate: $key -> $value")
         }
 
-        binding.tvOutput.text = prefs.getString("user_name","defaultwert")
+        if(setColor){
+            binding.root.setBackgroundColor(Color.RED)
+        }else{
+            binding.root.setBackgroundColor(Color.WHITE)
+        }
+
+        binding.tvOutput.text = prefs.getString("username", "defaultwert")
 
         binding.btnSetName.setOnClickListener {
-            prefs.edit().putString("user_name", binding.etName.text.toString())
-            .apply()
-            binding.tvOutput.text = prefs.getString("user_name","Default")
+            prefs.edit().putString("username", binding.etName.text.toString())
+                .apply()
+            binding.tvOutput.text = prefs.getString("username", "Default")
         }
 
         binding.btnDelete.setOnClickListener {
-            prefs.edit().remove("user_name").apply()
-            binding.tvOutput.text = prefs.getString("user_name","Default")
+            prefs.edit().remove("username").apply()
+            binding.tvOutput.text = prefs.getString("username", "Default")
         }
+
+
 
     }
 
+
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main,menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
-            R.id.action_settings ->{
-                startActivity(Intent(this,SettingsActivity::class.java))
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/example/settingsdemo/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/settingsdemo/settings/SettingsActivity.kt
@@ -1,44 +1,72 @@
 package com.example.settingsdemo.settings
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
+import androidx.preference.EditTextPreferenceDialogFragmentCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.settingsdemo.R
 
-class SettingsActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback{
+// Interface ist nicht notwendig, es dient dazu um von einer Preferenz zu anderen zu gelangen
+class SettingsActivity : AppCompatActivity(),
+    PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container_settings,RootFragment())
+            .replace(R.id.container_settings, RootFragment())
             .commit()
     }
 
+    // Funktion aus dem Interface
     override fun onPreferenceStartFragment(
         caller: PreferenceFragmentCompat,
         pref: Preference
     ): Boolean {
         val args = pref.extras
-        val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader,pref.fragment!!)
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+            classLoader,
+            pref.fragment!!
+        ) as PreferenceFragmentCompat
+        args.putString("name", "Frank")
         fragment.arguments = args
+        // Zeile ist notwendig,um den key zu prüfen
+        supportFragmentManager.setFragmentResult("Dialog Prefs", args)
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container_settings,fragment)
+            .replace(R.id.container_settings, fragment)
             .addToBackStack(null)
             .commit()
+        Log.d("TAG", "onPreferenceStartFragment:${pref.key} ")
+        supportFragmentManager.setFragmentResultListener(pref.key, fragment) { key, bundle ->
+            if (key == "Dialog Prefs") {
+// der username muss gesetzt sein, damit in den Dialog Prefs einstellungen vorgenommen werden können
+                if (caller.findPreference<EditTextPreference>("username")?.text == "empty") {
+                    onClick(caller)
+                    Toast.makeText(this, "Bitte erst den Usernamen festlegen", Toast.LENGTH_SHORT)
+                        .show()
+                }
 
-        supportFragmentManager.setFragmentResultListener("xy",fragment){key,bundle ->
-            if(key == "xy"){
-                supportFragmentManager.setFragmentResult("xy", bundle)
             }
         }
 
         return true
     }
 
+    fun onClick(caller: PreferenceFragmentCompat) {
+        val p = caller.preferenceManager.findPreference<EditTextPreference>("username")
+
+        val dialogFragment = EditTextPreferenceDialogFragmentCompat.newInstance(p!!.key)
+        dialogFragment.setTargetFragment(caller, 0) // hier muss ein anderer Weg gefunden werden, das RootFragment zu benutzen
+        dialogFragment.show(caller.parentFragmentManager, p.key)
+        Log.d("TAG", "onClick: ich bin drin ${dialogFragment.targetFragment!!.javaClass.simpleName}")
+    }
+
     override fun onSupportNavigateUp(): Boolean {
-        if(supportFragmentManager.popBackStackImmediate()){
+        if (supportFragmentManager.popBackStackImmediate()) {
             return true
         }
         return super.onSupportNavigateUp()
